e3b67f7d6cf9164a7a2c371620a85d74f19e0b54
Dariush Saberi <48084415+dariush-saberi@users.noreply.github.com>
2021-03-17 12:48:12 +0800

    Update setup.py

--- acadnomClass/setup.py
+++ acadnomClass/setup.py
@@ -1,16 +1,17 @@
 from distutils.core import setup
 from distutils.extension import Extension
+
 from Cython.Distutils import build_ext
 
-ext_modules=[
-    Extension("langmodel",         ["langmodel.pyx"]),
-    Extension("nom",       ["nominalizerV8N.pyx"]),
-    Extension("acadz",         ["academizerV3.pyx"]),
-    Extension("freephrase",         ["freephrase.pyx"]),
+ext_modules = [
+    Extension("langmodel", ["langmodel.pyx"]),
+    Extension("nom", ["nominalizerV8N.pyx"]),
+    Extension("acadz", ["academizerV3.pyx"]),
+    Extension("freephrase", ["freephrase.pyx"]),
 ]
 
 setup(
-  name = 'FormalWriter',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules,
+    name="FormalWriter",
+    cmdclass={"build_ext": build_ext},
+    ext_modules=ext_modules,
 )
